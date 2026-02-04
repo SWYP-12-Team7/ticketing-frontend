9e33e6d0722817a544a6c503ab35cf9b358db4b0
youngsoon12 <enejwk789@gmail.com>
2026-02-05 08:09:08 +0900

    fix: max-width: 1280 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,13 +48,15 @@ export default function RootLayout({
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
         />
       </head>
-      <body className="flex min-h-screen flex-col font-pretendard">
+      <body className="font-pretendard">
         <Providers>
-          <Suspense fallback={null}>
-            <Header />
-          </Suspense>
-          <main className="flex-1 px-[80px]">{children}</main>
-          <Footer />
+          <div className="mx-auto flex min-h-screen max-w-[1280px] flex-col border-1">
+            <Suspense fallback={null}>
+              <Header />
+            </Suspense>
+            <main className="flex-1 px-[80px]">{children}</main>
+            <Footer />
+          </div>
         </Providers>
       </body>
     </html>
