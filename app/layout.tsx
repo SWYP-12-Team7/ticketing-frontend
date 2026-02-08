56b4696c5c8726395f973b8ed514c12ce960211e
youngsoon12 <enejwk789@gmail.com>
2026-02-08 23:52:17 +0900

    feat: toast 추가 및 정렬 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Providers from "./providers";
 import { Header } from "@/components/common/Header";
 import { Footer } from "@/components/common/Footer";
+import { Toaster } from "sonner";
 
 // ===== 폰트 설정 =====
 
@@ -59,6 +60,7 @@ export default function RootLayout({
           <main className="flex-1 px-[80px] max-w-[1440px] mx-auto ">{children}</main>
           <Footer />
         </Providers>
+        <Toaster position="top-right" richColors />
       </body>
     </html>
   );
