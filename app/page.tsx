b3b1b7e579d84b74895459e36c4136bc248732f6
youngsoon12 <enejwk789@gmail.com>
2026-02-21 00:49:02 +0900

    style: 회색 배경 제거

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         />
 
         {/* 오픈 예정 행사 */}
-        <div className="-mx-[80px] bg-[#F3F4F6] px-[80px] py-6">
+        <div className="py-6">
           <ShowPick
             title="이번 주 안에 끝나요!"
             subtitle="사전 예약하고 특별한 혜택이?"
@@ -74,7 +74,7 @@ export default function Home() {
         <AdBanner className="py-6" />
 
         {/* 전문가 PICK 섹션 */}
-        <div className="-mx-[80px] bg-[#F3F4F6] px-[80px] py-6">
+        <div className="py-6">
           <ShowPick
             title="따끈따끈! 방금 오픈한 행사를 만나보세요!"
             subtitle="누구보다 빠르게 새로운 행사를 경험하세요"
